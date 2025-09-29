b69954a927893babff3a0422aaacfe0262a8a682
sounkou-bioinfo <sounkoutoure@gmail.com>
2025-09-29 21:24:36 +0400

    vanilla

--- R/server.R
+++ R/server.R
@@ -1,3 +1,10 @@
+#' Serve static files for DuckDB-Wasm web apps using Go HTTP server
+#'
+#' @param static_dir Directory to serve (default: inst/static in current working directory)
+#' @param goserver_addr Address to bind the server (default: "127.0.0.1:8081")
+#' @param prefix URL prefix for served files (default: "/static")
+#' @return None. Blocking call. Cleans up mtcars.csv on exit.
+#' @export
 server <- function(static_dir = file.path(getwd(), "inst/static"),
                    goserver_addr = "127.0.0.1:8081",
                    prefix = "/static") {
